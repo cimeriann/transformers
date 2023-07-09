81793fbcc585d189b229460e29295708c3842b45
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-07-09 20:29:00 +0100

    getting started with pandas

--- numpy/arrays.py
+++ numpy/arrays.py
@@ -55,3 +55,12 @@ a = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 # print(a.T)
 # print(np.linalg.inv(a))
 # print(np.linalg.det(a))
+
+# slicing
+print(a)
+print(a[0, 0:3])
+
+print(a[-1, -1])
+
+bool_idx = a > 2
+print(bool_idx)
